edb34450fb88238e7692bb21853ca7253a144048
XiaoHeiKEN <xiaoheiken@gmail.com>
2019-08-11 19:18:58 +0800

    Update User.java

--- CryptoMarket/src/cryptomarket/User.java
+++ CryptoMarket/src/cryptomarket/User.java
@@ -1,43 +1,81 @@
 package cryptomarket;
+
 public class User {
+
+    private String userID;
+    private int userPswrd;
     private String name;
     private String publicAddress;
     private String privateAddress;
     private Wallet wallet;
     private double accountBalance;
-    
-    /***
+
+    /**
+     * *
      * User no arg-constructor
      */
     public User() {
-    this.name = "";
-    this.publicAddress = "";
-    this.privateAddress = "";
-    this.wallet = new Wallet();
-    this.accountBalance = 0;
+        this.userID = "";
+        this.userPswrd = 0;
+        this.name = "";
+        this.publicAddress = "";
+        this.privateAddress = "";
+        this.wallet = new Wallet();
+        this.accountBalance = 0;
     }
-    
-    /***
-     * 
+
+    /**
+     * *
+     * @param  userID ID of user
      * @param name user name
      * @param publicAddress user wallet public address
      * @param privateAddress user wallet private address
      * @param wallet user wallet with coins inside
      * @param accountBalance user account balance
      */
-public User (String name, String publicAddress, String privateAddress, Wallet wallet, double accountBalance) {
-    this.name = name;
-    this.publicAddress = publicAddress;
-    this.privateAddress = privateAddress;
-    this.wallet = wallet;
-    this.accountBalance = accountBalance;
+    public User(String userID, int userPswrd, String name, String publicAddress, String privateAddress, Wallet wallet, double accountBalance) {
+        this.userPswrd=userPswrd;
+        this.userID = userID;
+        this.name = name;
+        this.publicAddress = publicAddress;
+        this.privateAddress = privateAddress;
+        this.wallet = wallet;
+        this.accountBalance = accountBalance;
     }
 
     @Override
     public String toString() {
-    return "Name: " + name + "\nAccount Balance: " +String.format("%.2f", accountBalance)
-     + "\nPublic Wallet Address: " + publicAddress + "\nPrivate Wallet Address: " + privateAddress;
-}
+        return "userID:" + userID +"\nuserPswrd:" + userPswrd + "Name: " + name + "\nAccount Balance: " + String.format("%.2f", accountBalance)
+                + "\nPublic Wallet Address: " + publicAddress + "\nPrivate Wallet Address: " + privateAddress;
+    }
+
+    /**
+     * @return the userID
+     */
+    public String getuserID() {
+        return userID;
+    }
+
+    /**
+     * @param userID the userID to set
+     */
+    public void setuserID(String userID) {
+        this.userID = userID;
+    }
+    
+    /**
+     * @return the userPswrd
+     */
+    public int getuserPswrd() {
+        return userPswrd;
+    }
+
+    /**
+     * @param userPswrd the userPswrd to set
+     */
+    public void setuserPswrd(int userPswrd) {
+        this.userPswrd = userPswrd;
+    }
 
     /**
      * @return the name
